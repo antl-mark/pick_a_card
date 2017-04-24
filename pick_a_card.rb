50f276f21d8113c151d25ad5177239b65bf916d0
antl-mark <a.markevuch@ukr.net>
2017-04-24 21:57:02 +0300

    Update coment in pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,5 +2,5 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
-#Choose randome couple
+#Choose randome couple of two cards
 puts "#{values.sample} of #{suits.sample}"
